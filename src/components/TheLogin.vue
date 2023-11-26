d32d6f4453e7e124018f110af44c5447bbfc71e8
Alexey5689 <jjookkeerrr56@gmail.com>
2023-11-26 15:51:52 +0500

    logIn/logOut+pinia

--- src/components/TheLogin.vue
+++ src/components/TheLogin.vue
@@ -1,9 +1,76 @@
 <template>
-  <div>Login</div>
+  <div class="loginWrapper">
+    <div v-if="!test.getLogin" class="login">
+      <div class="login_Head">
+        <h1>Вход</h1>
+      </div>
+      <div class="ruls">
+        <p>{{ test.getShowResponse }}</p>
+      </div>
+      <form @submit.prevent="test.LogForm" class="login_form">
+        <div class="login_form_inputs">
+          <input
+            type="text"
+            placeholder="User*"
+            class="user"
+            v-model="test.user"
+          />
+          <div class="passwordInput">
+            <input
+              v-if="test.getShowPass"
+              type="password"
+              placeholder="Пароль*"
+              class="password"
+              v-model="test.password"
+            />
+            <input
+              v-else
+              type="text"
+              placeholder="Пароль*"
+              class="password"
+              v-model="test.password"
+            />
+            <div
+              class="show open"
+              @click="test.showPassword"
+              :class="{ hidden: !test.getShowPass }"
+            ></div>
+          </div>
+        </div>
+        <div class="login_Button">
+          <div class="login_Btn">
+            <button type="submit">Войти</button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div v-else class="login">
+      <div class="login_Head">
+        <h1>Вход</h1>
+      </div>
+      <div class="login_Button">
+        <form @submit.prevent="test.LogOutForm" class="login_form">
+          <div class="login_Button">
+            <div class="login_Btn">
+              <button @click="test.LogOutForm" type="submit">Выйти</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import { testProject } from "@/stores/index";
 export default {
   name: "Login",
+  setup() {
+    const test = testProject();
+    return {
+      test,
+    };
+  },
 };
 </script>
-<style scoped></style>
+<style src="@/components/componentStyle/login/loginStyle.css" scoped></style>
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,1 +1,59 @@
-import {defineStore} from 'pinia';
+import { defineStore } from "pinia";
+export const testProject = defineStore("test", {
+  state: () => ({
+    user: "",
+    password: "",
+    showPass: true,
+    response: "",
+    IsLogin: false,
+  }),
+  getters: {
+    getShowPass() {
+      return this.showPass;
+    },
+    getShowResponse() {
+      return this.response;
+    },
+    getLogin() {
+      return this.IsLogin;
+    },
+  },
+  persist: {
+    enabled: true,
+    strategies: [
+      {
+        key: "login",
+        storage: localStorage,
+        paths: ["IsLogin"],
+      },
+    ],
+  },
+  actions: {
+    showPassword() {
+      this.showPass = !this.showPass;
+    },
+    LogForm() {
+      if (this.user === "" || this.password === "") {
+        this.response = "Поля должны быть заполнены";
+        setTimeout(() => {
+          this.response = "";
+          this.user = "";
+          this.password = "";
+        }, 2000);
+      } else if (this.user !== "admin" || this.password !== "admin") {
+        this.response = "Неверный логин или пароль";
+        setTimeout(() => {
+          this.response = "";
+          this.user = "";
+          this.password = "";
+        }, 2000);
+      } else {
+        this.IsLogin = true;
+        window.location.href = "/profile";
+      }
+    },
+    LogOutForm() {
+      this.IsLogin = false;
+    },
+  },
+});
